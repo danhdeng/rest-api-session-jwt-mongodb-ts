7ff5970e5b0d48eab9304d23df1606a6945c1bab
Dan Deng <hui_danny_deng@hotmail.com>
2022-01-07 14:01:22 -0500

    update product model and schema

---
+++ src/models/product.model.ts
@@ -0,0 +1,36 @@
+import {
+    getModelForClass,
+    prop,
+    Ref,
+    modelOptions,
+} from '@typegoose/typegoose';
+import { nanoid } from 'nanoid';
+import { User } from '@/models/user.model';
+
+@modelOptions({
+    schemaOptions: {
+        timestamps: true,
+    },
+})
+export class Product {
+    @prop({
+        required: true,
+        unique: true,
+        default: () => `product_${nanoid()}`,
+    })
+    productId: string;
+    @prop({ ref: () => User })
+    user: Ref<User>;
+    @prop({ required: true })
+    title: string;
+    @prop({ required: true })
+    description: string;
+    @prop({ required: true })
+    price: number;
+    @prop({ required: true })
+    image: string;
+}
+
+const ProductModel = getModelForClass(Product);
+
+export default ProductModel;
